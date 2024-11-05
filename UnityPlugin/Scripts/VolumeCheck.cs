53326247f12cf3ead1f1c5fa9c2ac0ea01c51f66
Kim Vo <126284919+KimVVo@users.noreply.github.com>
2024-11-05 00:52:33 -0600

    Update VolumeCheck.cs

--- UnityPlugin/Scripts/VolumeCheck.cs
+++ UnityPlugin/Scripts/VolumeCheck.cs
@@ -1,45 +1,7 @@
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEditor;
-using System.Collections.Generic;
 
-public class VolumeProperties
-{
-    public int masterVolume;
-    public int voiceVolume;
-    public int musicVolume;
-    public int sfxVolume;
-
-    // assign {1 - too quiet, 2 - too loud, 3 - safe}
-    public int masterPass;
-    public int voicePass;
-    public int musicPass;
-    public int sfxPass;
-
-    public string masterSuggestion;
-    public string voiceSuggestion;
-    public string musicSuggestion;
-    public string sfxSuggestion;
-
-    public VolumeProperties(int masterVolume, int voiceVolume, int musicVolume, int sfxVolume, 
-                            int masterPass, int voicePass, int musicPass, int sfxPass,
-                            string masterSuggestion, string voiceSuggestion, string musicSuggestion, string sfxSuggestion)
-    {
-        this.masterVolume = masterVolume;
-        this.voiceVolume = voiceVolume;
-        this.musicVolume = musicVolume;
-        this.sfxVolume = sfxVolume;
-        this.masterPass = masterPass;
-        this.voicePass = voicePass; 
-        this.musicPass = musicPass;
-        this.sfxPass = sfxPass;
-        this.masterSuggestion = masterSuggestion;
-        this.voiceSuggestion = voiceSuggestion;
-        this.musicSuggestion = musicSuggestion;
-        this.sfxSuggestion = sfxSuggestion;
-    }   
-
-}
 public class VolumeChecker
 {
     public AudioMixer audioMixer;
@@ -55,24 +17,6 @@ public class VolumeChecker
     private const float sfxMinLevel = -12f;
     private const float sfxMaxLevel = -3f;
 
-    // assign {0 - no sound found, 1 - too quiet, 2 - too loud, 3 - safe}
-    int masterPass = 0;
-    int voicePass = 0;
-    int musicPass = 0;
-    int sfxPass = 0;
-
-    string masterSuggestion;
-    string voiceSuggestion;
-    string musicSuggestion;
-    string sfxSuggestion;
-
-
-    public List<VolumeProperties> volumePropertiesList = new List<VolumeProperties>();  
-
-    public void OnEnable ()
-    {
-        CheckVolumeLevels();
-    }
     public void OnGUI()
     {
         EditorGUILayout.LabelField("Volume Checker", EditorStyles.boldLabel);
@@ -119,15 +63,6 @@ public class VolumeChecker
         result += GetLoudnessResult(voiceVolume, "Voice", Mathf.RoundToInt(voiceMinLevel), Mathf.RoundToInt(voiceMaxLevel));
         result += GetLoudnessResult(musicVolume, "Music", Mathf.RoundToInt(musicMinLevel), Mathf.RoundToInt(musicMaxLevel));
         result += GetLoudnessResult(sfxVolume, "SFX", Mathf.RoundToInt(sfxMinLevel), Mathf.RoundToInt(sfxMaxLevel));
-
-        masterSuggestion = GetLoudnessResult(masterVolume, "Master", Mathf.RoundToInt(masterMinLevel), Mathf.RoundToInt(masterMaxLevel));
-        voiceSuggestion = GetLoudnessResult(voiceVolume, "Voice", Mathf.RoundToInt(voiceMinLevel), Mathf.RoundToInt(voiceMaxLevel));
-        musicSuggestion = GetLoudnessResult(musicVolume, "Music", Mathf.RoundToInt(musicMinLevel), Mathf.RoundToInt(musicMaxLevel));
-        sfxSuggestion = GetLoudnessResult(sfxVolume, "SFX", Mathf.RoundToInt(sfxMinLevel), Mathf.RoundToInt(sfxMaxLevel));
-
-        volumePropertiesList.Add(new VolumeProperties(masterVolume, voiceVolume, musicVolume, sfxVolume, 
-                                                      masterPass, voicePass, musicPass, sfxPass,
-                                                      masterSuggestion, voiceSuggestion, musicSuggestion, sfxSuggestion));
     }
 
     private float GetAudioMixerVolume(string exposedParameter)
@@ -148,26 +83,14 @@ public class VolumeChecker
     {
         if (volume < minLevel)
         {
-            masterPass = 1;
-            voicePass = 1;
-            musicPass = 1;
-            sfxPass = 1;
             return $"{type} volume is too quiet! Consider raising the volume.\n";
         }
         else if (volume > maxLevel)
         {
-            masterPass = 2;
-            voicePass = 2;
-            musicPass = 2;
-            sfxPass = 2;
             return $"{type} volume is too loud! Consider lowering the volume.\n";
         }
         else
         {
-            masterPass = 3;
-            voicePass = 3;
-            musicPass = 3;
-            sfxPass = 3;
             return $"{type} volume is at a safe level.\n";
         }
     }
