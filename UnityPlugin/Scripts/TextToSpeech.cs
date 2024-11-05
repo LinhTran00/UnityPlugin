d6ca5e761041bf33fa8d825828b0ba32ae2b2016
Kim Vo <126284919+KimVVo@users.noreply.github.com>
2024-11-05 00:50:27 -0600

    Add files via upload
    Adding TextToSpeech.cs

---
+++ UnityPlugin/Scripts/TextToSpeech.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Amazon.Polly;
+using Amazon.Runtime;
+using Amazon;
+using Amazon.Polly.Model;
+using System.IO;
+using UnityEngine.Networking;
+using System.Threading.Tasks;
+
+public class TextToSpeech : MonoBehaviour
+{
+    [SerializeField] private AudioSource audioSource;
+    private AmazonPollyClient client;
+
+    private void Start()
+    {
+        var credentials = new BasicAWSCredentials(accessKey: "add-your-access-key", secretKey: "add-your-secret-key");
+        client = new AmazonPollyClient(credentials, RegionEndpoint.USEast1);
+    }
+
+    public async void Speak(string text)
+    {
+        var request = new SynthesizeSpeechRequest()
+        {
+            Text = text,
+            Engine = Engine.Neural,
+            VoiceId = VoiceId.Aria,
+            OutputFormat = OutputFormat.Mp3
+        };
+
+        var response = await client.SynthesizeSpeechAsync(request);
+
+        WriteIntoFile(response.AudioStream);
+
+        using (var www = UnityWebRequestMultimedia.GetAudioClip($"{Application.persistentDataPath}/audio.mp3", AudioType.MPEG))
+        {
+            var op = www.SendWebRequest();
+            while (!op.isDone) await Task.Yield();
+
+            var clip = DownloadHandlerAudioClip.GetContent(www);
+
+            audioSource.clip = clip;
+            audioSource.Play();
+        }
+    }
+
+    private void WriteIntoFile(Stream stream)
+    {
+        using (var fileStream = new FileStream($"{Application.persistentDataPath}/audio.mp3", FileMode.Create))
+        {
+            byte[] buffer = new byte[8 * 1024];
+            int bytesRead;
+
+            while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                fileStream.Write(buffer, 0, bytesRead);
+            }
+        }
+    }
+}
